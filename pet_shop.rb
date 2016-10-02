6e196c3c207cdf75e12c023f8edaad312affc864
Max <mveasey87@gmail.com>
2016-10-02 10:28:34 +0100

    15 tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,25 +2,31 @@ def pet_shop_name(shop_name)
   shop_name[:name]
 end
 
+
 def total_cash(ammount)
   ammount[:admin][:total_cash]
 end
+
 
 def add_or_remove_cash(ammount, cash)
   ammount[:admin][:total_cash] += cash
 end
 
+
 def pets_sold(pet_shop)
   pet_shop[:admin][:pets_sold]
 end
+
 
 def increase_pets_sold(pet_shop, pets_sold)
   pet_shop[:admin][:pets_sold] += pets_sold
 end
 
+
 def stock_count(pet_shop)
   pet_shop[:pets].count
 end
+
 
 def pets_by_breed(pet_shop, breed)
   
@@ -34,6 +40,7 @@ def pets_by_breed(pet_shop, breed)
 
 end
 
+
 def find_pet_by_name(pet_shop, pet_name)
 
   pets = []
@@ -46,19 +53,28 @@ def find_pet_by_name(pet_shop, pet_name)
 
 end
 
- def remove_pet_by_name(pet_shop, pet_name) 
 
+def remove_pet_by_name(pet_shop, pet_name) 
+  
   for pet in pet_shop[:pets]
     if pet[:name] == pet_name
       pet_shop[:pets].delete(pet)
     end
   end
-
- end
+  
+end
+  
 
 def add_pet_to_stock(pet_shop, new_pet)
-
   pet_shop[:pets].push(new_pet)
-
 end
 
+
+def customer_pet_count(customer)
+  customer[:pets].length
+end
+
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
